6aad8e61adb86f20aeb2884708c54aa3e221aa3d
gastoire <gastonmm@gmail.com>
2023-01-31 16:59:25 -0300

    feat: endpoint GET para enums

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,8 +6,13 @@ import {
   Body,
   Put,
   Delete,
+  UseGuards
 } from '@nestjs/common';
+import { AuthGuard } from './auth.guard';
 import { AppService } from './app.service';
+
+
+import { Role, Tracing, Priority } from '@prisma/client';
 
 @Controller()
 export class AppController {
@@ -16,5 +21,23 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('roles')
+  @UseGuards(AuthGuard)
+  getRoles(): object {
+    return Role;
+  }
+
+  @Get('tracings')
+  @UseGuards(AuthGuard)
+  getTracings(): object {
+    return Tracing;
+  }
+
+  @Get('priorities')
+  @UseGuards(AuthGuard)
+  getPriorities(): object {
+    return Priority;
   }
 }
