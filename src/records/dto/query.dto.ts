95f4f19664b5db8c4ac03efac5598faced2aaacd
gastoire <gastonmm@gmail.com>
2023-02-15 06:32:45 -0300

    fix: filter records

--- src/records/dto/query.dto.ts
+++ src/records/dto/query.dto.ts
@@ -12,25 +12,25 @@ import { Type } from 'class-transformer';
 import { Prisma, Tracing, Priority, Note } from '@prisma/client';
 
 export class QueryDto {
-  @IsEnum(Tracing)
-  @IsOptional()
-  @IsNotEmpty()
-  tracing: Tracing;
-
-  @IsEnum(Priority)
-  @IsOptional()
-  @IsNotEmpty()
-  priority: Priority;
-
-  @IsBoolean()
-  @IsOptional()
-  @IsNotEmpty()
-  archive: boolean;
-
   @IsString()
   @IsOptional()
   @IsNotEmpty()
   search: string;
+
+  @IsEnum(Prisma.SortOrder)
+  @IsOptional()
+  @IsNotEmpty()
+  name: Prisma.SortOrder;
+
+  @IsEnum(Prisma.SortOrder)
+  @IsOptional()
+  @IsNotEmpty()
+  order: Prisma.SortOrder;
+
+  @IsEnum(Prisma.SortOrder)
+  @IsOptional()
+  @IsNotEmpty()
+  favorite: Prisma.SortOrder;
 
   @IsEnum(Prisma.SortOrder)
   @IsOptional()
